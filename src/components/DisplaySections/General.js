13ca0aa4a604b4df350bc2a4c1d4cd02ac1c5c6d
TiagoJacintoDev <tiagojacintodev@gmail.com>
2022-09-07 10:15:16 +0200

    Implement Print CV Button

---
+++ src/components/DisplaySections/General.js
@@ -0,0 +1,37 @@
+import { BsTelephone } from 'react-icons/bs';
+import { TiMail } from 'react-icons/ti';
+import { FiGithub } from 'react-icons/fi';
+import { AiOutlineLinkedin } from 'react-icons/ai';
+
+export default function General({ formData }) {
+  const { general } = formData;
+  const { firstName, lastName, occupation, phone, email, github, linkedin } =
+    general[0];
+
+  return (
+    <div className='display-section'>
+      <div className='flex space-between align-center'>
+        <div className='display-info'>
+          <h1 className='fs-800 fw-bold'>
+            {firstName || 'John'} {lastName || 'Doe'}
+          </h1>
+          <p>{occupation || 'Occupation'}</p>
+        </div>
+        <div>
+          <p>
+            <BsTelephone /> {phone || 'Phone'}
+          </p>
+          <p>
+            <TiMail /> {email || 'Email'}
+          </p>
+          <p>
+            <FiGithub /> {github || 'Github'}
+          </p>
+          <p>
+            <AiOutlineLinkedin /> {linkedin || 'LinkedIn'}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
